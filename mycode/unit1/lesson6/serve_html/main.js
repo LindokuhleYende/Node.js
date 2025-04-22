b1b7bc9c545784afbfed2ae154216fe638c4089e
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-22 09:53:49 +0200

    Update main.js

--- mycode/unit1/lesson6/serve_html/main.js
+++ mycode/unit1/lesson6/serve_html/main.js
@@ -2,26 +2,34 @@
 
 const port = 3000,
     http = require("http"),
-    httpStatus = require("http-status-codes"),
-    fs = require("fs");
-const getViewUrl = url => {
-    return `views${url}.html`;
-};
-http
-    .createServer((req, res) => {
-        let viewUrl = getViewUrl(req.url);
-        fs.readFile(viewUrl, (error, data) => {
-            if (error) {
-                res.writeHead(httpStatus.NOT_FOUND);
-                res.write("<h1>FILE NOT FOUND</h1>");
-            } else {
-                res.writeHead(httpStatus.OK, {
-                    "Content-Type": "text/html"
-                });
-                res.write(data);
+    httpStatusCodes = require("http-status-codes"),
+    router = require("./router"),
+    fs = require("fs"),
+    plainTextContentType = {
+        "Content-Type": "text/plain"
+    },
+    htmlContentType = {
+        "Content-Type": "text/html"
+    },
+    customReadFile = (file, res) => {
+        fs.readFile(`./${file}`, (errors, data) => {
+            if (errors) {
+                console.log("Error reading the file...");
             }
-            res.end();
+            res.end(data);
         });
-    })
-    .listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+    };
+router.get("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("INDEX");
+});
+router.get("/index.html", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, htmlContentType);
+    customReadFile("views/index.html", res);
+});
+router.post("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("POSTED");
+});
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number: ${port}`);
