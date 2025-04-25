15bccaffce327600f4069162de5cdfa8f988b5a2
Lindokuhle <yendelindokuhle2@gmail.com>
2025-04-25 10:43:12 +0200

    Update errorController.js

--- mycode/unit2/lesson11/express_templates/controllers/errorController.js
+++ mycode/unit2/lesson11/express_templates/controllers/errorController.js
@@ -11,12 +11,13 @@ exports.logErrors = (error, req, res, next) => {
 exports.respondNoResourceFound = (req, res) => {
     let errorCode = httpStatus.NOT_FOUND;//404 error
     res.status(errorCode);
-    res.sendFile(`./public/${errorCode}.html`, { root: "./"});
+    res.sendFile(`./public/${errorCode}.html`, { root: "./" });
 };
 
 exports.respondInternalError = (req, res) => {
     let errorCode = httpStatus.INTERNAL_SERVER_ERROR;//500 error
     console.log(`ERROR occurred: ${error.stack}`)
     res.status(errorCode);
-    res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+    // res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+    res.sendFile("./public/500.html", { root: "./" })
 };
